be17eaf6ae6e8d5ca52148b5a9de09a4b5d66e3a
Emmanuel Bachlitzanakis <115871174+EmBachlitzanakis@users.noreply.github.com>
2024-11-06 14:32:54 +0200

    Add files via upload

---
+++ WerbServer/Server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Clifton.WebServer
+{
+    public static class Server
+    {
+        private static HttpListener listener;
+        public static int maxSimultaneousConnections = 20;
+        private static Semaphore sem = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
+
+        private static List<IPAddress> GetLocalHostIPs()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> ret = host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+            return ret;
+        }
+
+        private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Prefixes.Add("http://localhost:8000/");
+            
+            return listener;
+        }
+        private static void Start(HttpListener listener)
+        {
+            listener.Start();
+            Task.Run(() => RunServer(listener));
+        }
+        private static void RunServer(HttpListener listener)
+        {
+            while (true)
+            {
+                sem.WaitOne();
+                StartConnectionListener(listener);
+            }
+        }
+        private static async void StartConnectionListener(HttpListener listener)
+        {
+            // Wait for a connection. Return to caller while we wait.
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            // Release the semaphore so that another listener can be immediately started up.
+            sem.Release();
+
+            // We have a connection, do something...
+            string response = "Hello Browser!";
+            byte[] encoded = Encoding.UTF8.GetBytes(response);
+            context.Response.ContentLength64 = encoded.Length;
+            context.Response.OutputStream.Write(encoded, 0, encoded.Length);
+            context.Response.OutputStream.Close();
+        }
+
+        public static void Start()
+        {
+            List<IPAddress> localHostIPs = GetLocalHostIPs();
+            HttpListener listener = InitializeListener(localHostIPs);
+            Start(listener);
+        }
+    }
+}
